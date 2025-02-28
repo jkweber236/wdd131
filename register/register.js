f25699041e15477bc8a0cd71ee9e3930950d7abf
Jennifer <coo21040@byui.edu>
2025-02-27 18:05:36 -0700

    Refactored template functions into templates.js

--- register/register.js
+++ register/register.js
@@ -1,3 +1,5 @@
+import { participantTemplate, successTemplate } from "./templates.js";
+
 let participantCount = 1;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -16,48 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
    form.addEventListener("submit", submitForm);
 });
 
-function participantTemplate(count) {
-   return `
-      <section class="participant${count}">
-         <p>Participant ${count}</p>
-         <div class="item">
-            <label for="fname${count}"> First Name<span>*</span></label>
-            <input id="fname${count}" type="text" name="fname${count}" value="" required />
-         </div>
-         <div class="item activities">
-            <label for="activity${count}">Activity #<span>*</span></label>
-            <input id="activity${count}" type="text" name="activity${count}" />
-         </div>
-         <div class="item">
-            <label for="fee${count}">Fee ($)<span>*</span></label>
-            <input id="fee${count}" type="number" name="fee${count}" />
-         </div>
-         <div class="item">
-            <label for="date${count}">Desired Date <span>*</span></label>
-            <input id="date${count}" type="date" name="date${count}" />
-         </div>
-         <div class="item">
-            <p>Grade</p>
-            <select>
-               <option selected value="" disabled selected></option>
-               <option value="1">1st</option>
-               <option value="2">2nd</option>
-               <option value="3">3rd</option>
-               <option value="4">4th</option>
-               <option value="5">5th</option>
-               <option value="6">6th</option>
-               <option value="7">7th</option>
-               <option value="8">8th</option>
-               <option value="9">9th</option>
-               <option value="10">10th</option>
-               <option value="11">11th</option>
-               <option value="12">12th</option>
-            </select>
-         </div>
-         </section>
-      `;
-}
-
 function submitForm(event) {
    event.preventDefault();
 
@@ -73,12 +33,6 @@ function submitForm(event) {
 
    document.querySelector("form").style.display = "none";
    summaryElement.style.display = "block";
-}
-
-function successTemplate(info) {
-   return `
-      Thank you ${info.name} for registering. You have registered ${info.participants} participants and owe $${info.totalFees} in Fees.
-   `;
 }
 
 function totalFees() {
