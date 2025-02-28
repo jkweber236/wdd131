f9448b63147021c4f8e25beb683565aaf51a23a9
Jennifer <coo21040@byui.edu>
2025-02-27 17:20:33 -0700

    Completed steps 4 and 5 of registration form assignment

--- register/register.js
+++ register/register.js
@@ -1,5 +1,7 @@
-document.addEventListener("DOMContentLoaded", () =>{
-   let participantCount = 1;
+let participantCount = 1;
+
+document.addEventListener("DOMContentLoaded", () => {
+   const form = document.querySelector("form");
 
    const addButton = document.querySelector("#add");
    const participantsFieldset = document.querySelector(".participants");
@@ -10,6 +12,8 @@ document.addEventListener("DOMContentLoaded", () =>{
 
       addButton.insertAdjacentHTML("beforebegin", participantHTML);
    });
+
+   form.addEventListener("submit", submitForm);
 });
 
 function participantTemplate(count) {
@@ -52,4 +56,45 @@ function participantTemplate(count) {
          </div>
          </section>
       `;
+}
+
+function submitForm(event) {
+   event.preventDefault();
+
+   const total = totalFees();
+   const adultName = document.querySelector("#adult_name").value;
+
+   const summaryElement = document.querySelector("#summary");
+   summaryElement.innerHTML = successTemplate({
+      name: adultName,
+      participants: participantCount,
+      totalFees: total
+   });
+
+   document.querySelector("form").style.display = "none";
+   summaryElement.style.display = "block";
+}
+
+function successTemplate(info) {
+   return `
+      Thank you ${info.name} for registering. You have registered ${info.participants} participants and owe $${info.totalFees} in Fees.
+   `;
+}
+
+function totalFees() {
+   // the selector below lets us grab any element that has an id that begins with "fee"
+   let feeElements = document.querySelectorAll("[id^=fee]");
+   console.log(feeElements);
+   // querySelectorAll returns a NodeList. It's like an Array, but not exactly the same.
+   // The line below is an easy way to convert something that is list-like to an actual Array so we can use all of the helpful Array methods...like reduce
+   // The "..." is called the spread operator. It "spreads" apart the list, then the [] we wrapped it in inserts those list items into a new Array.
+   feeElements = [...feeElements];
+   // sum up all of the fees. Something like Array.reduce() could be very helpful here :) Or you could use a Array.forEach() as well.
+   const total = feeElements.reduce((sum, feeElement) => {
+   // Remember that the text that was entered into the input element will be found in the .value of the element.
+      const fee = Number(feeElement.value);
+      return sum + (fee || 0);
+   }, 0);
+   // once you have your total make sure to return it!
+   return total;
 }
